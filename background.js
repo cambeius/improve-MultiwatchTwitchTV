0ffcdafd0f96eb046b7d222dad8ebb28d7c57c1a
cambei <54690836+cambeius@users.noreply.github.com>
2022-07-14 14:45:06 +0300

    Create background.js

---
+++ background.js
@@ -0,0 +1,28 @@
+function moveChat(){
+    var chat = document.querySelector("#chatbox")
+        wrapper = document.querySelector('#wrapper')
+
+    wrapper.insertBefore(chat, wrapper.children[0]);
+
+        chat.style.width = "354px";
+        chat.style.float = "left";
+        chat.style.marginLeft = "-8px";
+        console.log("the current width is: "+ chat.style.width);
+
+    window.addEventListener('resize', function() {
+        chat.style.width = "354px";
+        console.log("the current width is: "+ chat.style.width);
+    });
+
+     
+}
+
+var checker = setInterval(() => {
+    if(document.readyState === "complete"){
+        console.log("page is ready to be fucked");
+        clearInterval(checker);
+        moveChat();
+        
+    }
+    else{console.log("not yet");}
+ }, 1000);
